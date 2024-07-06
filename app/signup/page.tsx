593fcd313429f63816ff3b0e05baa4e1372cbf98
rafaapcode <rafaap2003@gmail.com>
2024-07-06 17:38:13 -0300

    feat: creating the design of the signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const SignUp = () => {
+  return (
+    <main className="bg-main-black flex flex-col gap-8 lg:gap-0 lg:flex-row justify-between items-center h-screen p-3 md:p-9">
+      <div className="hidden h-full w-[30%] bg-white/10 lg:flex justify-center items-center rounded-3xl">
+        <div className="w-[248px] h-[203px] relative">
+          <Image alt="Logo CutNow" fill src="/logo.svg" className="object-fill"/>
+        </div>
+      </div>
+      <div className="hidden md:block lg:hidden w-[95px] h-[78px] relative">
+        <Image alt="Logo CutNow" fill src="/logo.svg" className="object-fill"/>
+      </div>
+      <div className="w-full lg:w-[67%] h-full bg-white/10 rounded-3xl  py-4 px-5 md:px-12">
+          <h1>Step Form HERE</h1>
+      </div>
+    </main>
+  )
+}
+
+export default SignUp;
