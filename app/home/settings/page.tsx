d422726e923851c6163b74877b1d552bc957831c
rafaapcode <rafaap2003@gmail.com>
2024-07-20 20:46:38 -0300

    feat: begin the settings page design

--- app/home/settings/page.tsx
+++ app/home/settings/page.tsx
@@ -1,8 +1,10 @@
-
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
-}
+    <section className="w-full h-full">
+      <h2 className="mt-11 ml-5 md:ml-0 xl:ml-5 text-title-3 md:text-title-2">Configurações</h2>
 
-export default Settings
+    </section>
+  );
+};
+
+export default Settings;
