6b94949d662f98adc3db5bb91470ebd194f5b2c2
rafaapcode <rafaap2003@gmail.com>
2024-08-14 09:02:36 -0300

    feat: creating the state type

--- context/authContext.ts
+++ context/authContext.ts
@@ -1,10 +1,36 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+type BarbershopType = {
+  informacoes: {
+    cep: string;
+    rua: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    numero: number;
+    horarioAbertura: string;
+    horarioFechamento: string;
+    fotosEstruturaBarbearia: string[];
+    fotoBanner: string;
+    logo: string;
+    status: string;
+  } | null;
+  servicos: {nomeService: string; tempoMedio: number; preco: number;}[];
+  id: string;
+  email: string;
+  nome: string;
+  nomeDaBarbearia: string;
+  cnpj: string;
+  lat: string;
+  lng: string;
+  isAdm: boolean;
+};
+
 type AuthState = {
   user: any;
   setUser: (newUser: any) => void;
-}
+};
 
 export const useAuthStore = create<AuthState, [["zustand/persist", AuthState]]>(
   persist(
@@ -13,7 +39,7 @@ export const useAuthStore = create<AuthState, [["zustand/persist", AuthState]]>(
       setUser: (newUser: any) => set({ user: newUser }),
     }),
     {
-      name: 'user-data'
+      name: "user-data",
     }
   )
 );
