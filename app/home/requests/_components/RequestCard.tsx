13be2abfb8f4cdbc61a91a301ec7537be2206871
rafaapcode <rafaap2003@gmail.com>
2024-10-27 07:40:28 -0300

    feat: fechting all the requests

--- app/home/requests/_components/RequestCard.tsx
+++ app/home/requests/_components/RequestCard.tsx
@@ -12,6 +12,9 @@ type IRequestCardProp = {
 
 const RequestCard = ({avatar, barbeiro_id, data, emailCliente, id, nomeCliente, tipoServico }: IRequestCardProp) => {
   console.log(id, barbeiro_id);
+  const hourDate = data.split("T");
+  const hour = hourDate[1];
+  const date = hourDate[0];
   return (
     <div className="w-[283px] h-[283px] md:w-[320px]  md:h-[320px] xl:w-[350px] xl:h-[350px] rounded-lg relative mx-auto">
       <Image
@@ -20,14 +23,14 @@ const RequestCard = ({avatar, barbeiro_id, data, emailCliente, id, nomeCliente, 
         src={avatar}
         className="object-cover rounded-lg"
       />
-      <div className="flex flex-col p-2 gap-y-2 absolute w-full bottom-0 rounded-b-lg h-[120px] bg-black/55 backdrop-blur-sm">
+      <div className="flex flex-col justify-between p-2 gap-y-2 absolute w-full bottom-0 rounded-b-lg h-[170px] bg-black/55 backdrop-blur-sm">
         <div className="bg-terciary-green p-2 w-fit rounded-md text-black font-semibold tracking-tight text-sm">
           <p>{tipoServico}</p>
         </div>
-        <div className="flex justify-between items-center font-bold">
-          <p className="text-base md:text-lg">{nomeCliente}</p>
-          <p className="text-sm md:text-base">{emailCliente}</p>
-          <p className="text-sm md:text-base">{data}</p>
+        <div className="flex flex-col gap-y-2 font-bold">
+          <p className="text-sm">{nomeCliente}</p>
+          <p className="text-sm">{emailCliente}</p>
+          <p className="text-xs">{date}  as  {hour}</p>
         </div>
         <div className="flex justify-between items-center">
           <button className="bg-neutral-500 text-sm text-white py-1 rounded-md font-bold px-2 text hover:bg-neutral-700 transition-all duration-150">RECUSAR</button>
--- app/home/requests/page.tsx
+++ app/home/requests/page.tsx
@@ -16,11 +16,10 @@ const Requests = () => {
   return (
     <section className="w-full mt-12">
       <h2 className="ml-5 md:ml-0 text-title-3 md:text-title-2">Solicitações de Agendamento</h2>
-      <div className="mt-5 pb-10 w-full h-[670px] bg-transparent grid grid-cols-1 xl:place-items-start xl:grid-cols-3 gap-x-12 md:gap-y-28 xl:gap-y-16 overflow-y-auto snap-y scroll-smooth">
       {error ? (
         <div className="mt-5 text-neutral-500">Ocorreu um Erro , por favor tente mais tarde.</div>
       ) : (
-        <div className="mt-5 pb-10 w-full h-[670px] bg-transparent grid grid-cols-1 place-items-center xl:place-items-start xl:grid-cols-3 gap-x-12 md:gap-y-28 xl:gap-y-16 overflow-y-auto snap-y scroll-smooth">
+        <div className="mt-5 pb-10 w-full h-[670px] bg-transparent grid grid-cols-1 place-items-center xl:place-items-start lg:grid-cols-3 gap-x-12 md:gap-y-28 xl:gap-y-16 overflow-y-auto snap-y scroll-smooth">
           {loading
             ?  <div className="w-[283px] h-[283px] md:w-[320px]  md:h-[320px] xl:w-[350px] xl:h-[350px] bg-neutral-800 animate-pulse rounded-lg relative mx-auto" />
             : data.allRequests.map((barberInfo: any) => (
@@ -37,7 +36,6 @@ const Requests = () => {
               ))}
         </div>
       )}
-      </div>
     </section>
   )
 }
